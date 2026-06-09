f66848fde81c3fe56effede5b7d3d83319cf11b2
Anjanesh Dubey <anjaneshdubey@gmail.com>
2026-06-09 22:17:47 +0530

    W2D2 Second commit

---
+++ Week2/Day2/StringPermutation.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+public class StringPermutation{
+
+    static boolean isFreqSame(int[] freq1, int[] freq2) {
+        for (int i = 0; i < 26; i++) {
+            if (freq1[i] != freq2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static boolean checkInclusion(String s1, String s2) {
+        int[] freq = new int[26];
+        for (int i = 0; i < s1.length(); i++) {
+            freq[s1.charAt(i) - 'a']++;
+        }
+        int windSize = s1.length();
+
+        for (int i = 0; i < s2.length(); i++) {
+
+            int windInd = 0;
+            int index = i;
+
+            int[] windFreq = new int[26];
+
+            while (windInd < windSize && index < s2.length()) {
+                windFreq[s2.charAt(index) - 'a']++;
+                windInd++;
+                index++;
+            }
+
+            if (isFreqSame(freq, windFreq)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+     Scanner ad = new Scanner(System.in);
+     String s1  =ad.next();
+     String s2 = ad.next();
+
+        boolean result = checkInclusion(s1, s2);
+
+        System.out.println(result);
+    }
+}
